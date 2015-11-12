f89e543fad91a7d1da5fd2c50c0e582b4062f93c
Eddy Espinal <eddy_espinal@condenast.com>
2015-11-12 08:45:10 -0500

    Add first test

---
+++ test/add.spec.js
@@ -0,0 +1,17 @@
+
+var expect = require('chai').expect;
+var sut = require('../lib/math');
+
+describe('math', function () {
+
+  describe('#add', function () {
+
+    var result = sut.add(1, 2);
+
+    it('adds the numbers', function () {
+      expect(result).to.eql(3);
+    });
+
+  })
+
+});
